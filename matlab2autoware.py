23de8df841e96ad718aa9bb2f35544f0e5cb2fbd
tabioka <tabioka@tabioka-HP-Z420-Workstation>
2020-06-19 10:57:56 +0800

    Tranform the matlab message.
    It is for reading a simple message and tranforming into autoware
    message type.
    
    TEST: Use `rostopic echo` to generate matlab message and check the output.

---
+++ matlab2autoware.py
@@ -0,0 +1,27 @@
+import argparse
+import rospy
+from autoware_msgs.msg import DetectedObject
+from autoware_msgs.msg import DetectedObjectArray
+from matlab_msgs.msg import DetectedBoundingBox
+from matlab_msgs.msg import DetectedBoundingBoxArray
+
+parser = argparse.ArgumentParser()
+parser.add_argument("matlab_topic")
+parser.add_argument("autoware_topic")
+args = parser.parse_args()
+
+pub = rospy.Publisher(args.autoware_topic, DetectedObjectArray, queue_size=10)
+
+def callback(box_array):
+  autoware_array = DetectedObjectArray()
+  for box in box_array.boxes:
+    autoware_object = DetectedObject()
+    autoware_object.pose = box.pose
+    autoware_object.dimensions = box.dimensions
+    autoware_array.objects.append(autoware_object)
+  pub.publish(autoware_array)
+
+
+rospy.init_node('matlab_proxy')
+rospy.Subscriber(args.matlab_topic, DetectedBoundingBoxArray, callback)
+rospy.spin()
